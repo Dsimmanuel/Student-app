af98cb38478804ed76810db90b8b9f0eb0d59721
Dsimmanuel <dsimmanuel592@gmail.com>
2022-07-08 11:46:13 +0530

    view-student component completed

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -12,7 +12,14 @@ export class ViewStudentComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  name="Akhil"
+  data=[
+    {"name":"Rahul","age":25,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"},
+    {"name":"Akhil","age":80,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"},
+    {"name":"Shibin","age":30,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"},
+    {"name":"Abhinav","age":47,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"},
+    {"name":"Shaheen","age":54,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"},
+    {"name":"Sudhil","age":31,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8-ixw2ZnQsPJj5GUxaRhyam0tbduUsbWJw&usqp=CAU"}
+  ]
 }
 
   
